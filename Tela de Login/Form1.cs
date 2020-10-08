c5bf99d484d7ebcace6d5c7e1bf25f8352587aed
Francisco das chagas <chagassilva06@hotmail.com>
2020-10-07 23:07:51 -0300

    Upload da Aplicação
    Pequena aplicação de acesso ao "um sistema "em C#

---
+++ Tela de Login/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Tela_de_Login
+{
+    public partial class Form1 : Form
+
+    {
+        Thread nt;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Francisco" && textBox2.Text == "35540033")
+            {
+                this.Close();
+                nt = new Thread(novoForm);
+                nt.SetApartmentState(ApartmentState.STA);
+                nt.Start();
+                //MessageBox.Show("Bem vindo ao Sistema!");
+            }
+            else
+            {
+
+                MessageBox.Show("Login ou Senha Incorreto!");
+            }
+
+        }
+
+        private void novoForm()
+        {
+            Application.Run(new Form2());
+
+
+            // esse codigo foi gerado quando eu criei o método "novoFor "throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tela de Login/Form2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tela_de_Login
+{
+    public partial class Form2 : Form
+    {  
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Console.ForegroundColor = ConsoleColor.Red;
+            //Console.BackgroundColor = ConsoleColor.Red;
+          
+            label1.Text = "Sistema Ativado!";
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Sistema Desativado!";        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
